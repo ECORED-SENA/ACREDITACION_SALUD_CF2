c864a1d6fd4175a6bdfd714578a9a04d437bc1e8
elrilacho <80546125+elrilacho@users.noreply.github.com>
2021-05-27 18:08:40 -0500

    Actividad
    Se cambia la actividad al menú lateral según solicitud de revisora

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -2,7 +2,6 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-tasks" titulo="Actividad didáctica")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    
     .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.tarjeta--azul.h-100.d-flex.align-items-center
@@ -17,9 +16,10 @@
             .col-sm.mb-3.mb-sm-0
               p Proceso de autoevaluación 
             .col-auto
-              a.boton.boton--b(:href="('/actividades/Ejecutable/story.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/Ejecutable/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
+    
 
 
 </template>
